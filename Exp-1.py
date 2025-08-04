0386b1087cc49755e168e69fa7936b4c14890461
TahirShaik <fahirf196@gmail.com>
2025-08-04 21:18:24 +0530

    Initial commit

---
+++ Exp-1.py
@@ -0,0 +1,14 @@
+import cv2
+image = cv2.imread(r"C:\Users\Fahir Shaik\OneDrive\Pictures\ChatGPT Image Jun 1, 2025, 03_57_06 PM.png")
+cv2.imshow('Original Image',image)
+cv2.waitKey(0)
+resized=cv2.resize(image,(300,300))
+cv2.imshow('Resized Image',resized)
+cv2.waitKey(0)
+cropped=image[100:400,200:500]
+cv2.imshow('Cropped Image',cropped)
+cv2.waitKey(0)
+rotated=cv2.rotate(image,cv2.ROTATE_90_CLOCKWISE)
+cv2.imshow('Rotated Image',rotated)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
