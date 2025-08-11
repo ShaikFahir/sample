e59abc0bc5238c9f6ac2a18dab4ba7488d4d970f
FahirShaik <fahirf196@gmail.com>
2025-08-11 10:00:22 +0530

    Fixed Face Detection

--- Exp-5.py
+++ Exp-5.py
@@ -1,11 +1,18 @@
 import cv2
+
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-image = cv2.imread(r"C:\Users\Fahir Shaik\Downloads\prabhas-baahubali-001.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+
+image=cv2.imread(r"C:\Users\Fahir Shaik\OneDrive\Pictures\wallpaper.png")  # Replace with actual path
+gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+faces=face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-cv2.imshow("Faces Detected", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+    cv2.imshow('Detected Faces :',image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
